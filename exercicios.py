f5c97f651065861be1075463eae0a091b4787b86
Projetos de Dados <thaancosta@gmail.com>
2024-08-01 15:24:47 -0300

    Fazendo os exercicios do bootcamp - inteiros

---
+++ exercicios.py
@@ -0,0 +1,27 @@
+# Inteiros (int)
+
+# Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# numero1 = int(input("Digite o primeiro número: "))
+# numero2 = int(input("Digite o segundo número: "))
+# soma_numeros = numero1 + numero2
+# print(soma_numeros)
+
+# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# numero_user = int(input("Digite o número: "))
+# divisor = 5
+# resto = numero_user % divisor
+# print(resto)
+
+# Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# numero1 = int(input("Digite o primeiro número: "))
+# numero2 = int(input("Digite o segundo número: "))
+# multiplicacao = numero1*numero2
+# print(multiplicacao)
+
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+divisao = numero1 / numero2
+print(int(divisao))
+
+# Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
